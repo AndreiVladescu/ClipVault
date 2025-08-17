0a537b3a6b29c88c8af8d897cba5b682bf8733e6
Andrei Vladescu <52881237+AndreiVladescu@users.noreply.github.com>
2025-08-17 09:03:40 -0700

    Solved unwrap_or_else error

--- src/tray.rs
+++ src/tray.rs
@@ -67,9 +67,7 @@ impl Tray {
             menu.append(&quit)?;
 
             let icon = load_icon_png(include_bytes!("../img/tray.png"))
-                .unwrap_or_else(|_| {
-                    anyhow::bail!("Failed to load tray icon. Ensure the image is a valid PNG.");
-                });
+                .unwrap();
 
             let tray_icon = TrayIconBuilder::new()
                 .with_tooltip("ClipVault")
